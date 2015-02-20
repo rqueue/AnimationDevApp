44c4042d73f3532eab75a1d78f672a123277dc68
Ryan Quan <ryan.quan@brigade.com>
2015-02-20 14:54:36 -0800

    Add more sample text to label previews

--- AnimationsDevApp/View Controllers/MaterializeLabelViewController.m
+++ AnimationsDevApp/View Controllers/MaterializeLabelViewController.m
@@ -23,7 +23,9 @@
 
     NSArray *strings = @[@"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.\n\n–Michael Jordan",
                          @"Every strike brings me closer to the next home run.\n\n–Babe Ruth",
-                         @"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.\n\n–Mark Twain"];
+                         @"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.\n\n–Mark Twain",
+                         @"The mind is everything. What you think you become.\n\n–Buddha",
+                         @"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.\n\n–Maya Angelou"];
 
     NSMutableArray *mutableTexts = [NSMutableArray array];
     for (NSString *string in strings) {
