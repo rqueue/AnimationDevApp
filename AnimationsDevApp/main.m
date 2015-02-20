91af4769f6fe43ed02d382d8f7d27e0b32b8327f
Ryan Quan <ryan.quan@brigade.com>
2015-02-20 14:03:43 -0800

    Initial commit
    This commit creates the project and removes the story board.

--- AnimationsDevApp/main.m
+++ AnimationsDevApp/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  AnimationsDevApp
-//
-//  Created by Ryan Quan on 2/20/15.
-//  Copyright (c) 2015 Ryan Quan. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
