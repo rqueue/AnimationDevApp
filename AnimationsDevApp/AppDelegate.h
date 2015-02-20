a367b35b907f012203abc6054affdcd88fc5097f
Ryan Quan <ryan.quan@brigade.com>
2015-02-20 13:57:51 -0800

    Initial Commit

---
+++ AnimationsDevApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationsDevApp
+//
+//  Created by Ryan Quan on 2/20/15.
+//  Copyright (c) 2015 Ryan Quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationsDevApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationsDevApp
+//
+//  Created by Ryan Quan on 2/20/15.
+//  Copyright (c) 2015 Ryan Quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
